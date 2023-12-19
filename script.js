504f630bf64d7a09df161b58dfdb9ddb3d9cfc05
sajna <sajnak9809.com>
2023-12-19 10:36:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const chatInput=document.querySelector("chat-input textarea");
+const sendChatBtn=document.querySelector("chat-input span");
+const chatbox=document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY="";
+
+const createChatLi=(message,className)=>{
+    //create a chat <li> element with passed message and classname
+    const chatLi= document.createElement("li");
+    chatLi.classList.add("chat",className);
+    let chatContent =className==="outgoing"?`<p>${message}</p>`:` <span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatLi.innerHTML=chatContent;
+    return chatLi;
+}
+const generateResponse=() =>{
+    const API_URL="https://api.openai.com/v1/chat/completions";
+    const requestOptions ={
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${API_KEY}`
+        },
+        body:JSON.stringify({
+            model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: userMessage }]
+      
+      
+        })
+    }
+}
+
+const handleChat=()=>{
+    userMessage=chatInput.ariaValueMax.trim();
+    if(!userMessage)return;
+
+    //append the user's message to the chatbox
+
+    chatbox.appendChild(createChatLi(userMessage,"outgoing"));
+
+    setTimeout(()=>{
+        //display "thinking..." message while waiting for the response
+    chatbox.appendChild(createChatLi("Thinking...","incoming"));
+    generateResponse();
+
+    },600);
+}
+sendChatBtn.addEventListener("click",handleChat);
